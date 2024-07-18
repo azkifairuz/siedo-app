e1d07d6dfa404b057cfcaccf2911924be96b158b
azkia ajmal fairuz <azkiajmal@gmail.com>
2024-07-19 02:57:59 +0700

    component base input

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,38 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-import PrimaryBtn from '@/components/PrimaryButton.vue'
+import BaseInputField from '@/components/BaseInpuField.vue'
+import { reactive } from 'vue';
+
+const tes = reactive({
+  name:"a",
+  password:""
+})
+
+function btnTes() {
+  console.log(tes);
+}
 
 </script>
 
 <template>
-  <div class="font-quicksand">
+  <div class="font-quicksand mx-10">
     <RouterView />
+    <h1>{{ tes.name }}</h1>
+
+    <BaseInputField
+     label="Username"
+     placeholder="masukan nidn anda"
+     v-model="tes.name"
+     type="text"
+    />
+    <BaseInputField
+     label="Password"
+     placeholder="masukan password anda"
+     v-model="tes.password"
+     type="text"
+    />
   </div>
+  <button @click="btnTes" class="bg-secondary p-8 m-5">tes</button>
 </template>
 
 <style scoped></style>
---
+++ src/components/BaseInpuField.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+defineProps<{
+    label: string
+    placeholder: string
+    type: string
+    modelValue: string
+}>()
+
+const emits = defineEmits(['update:modelValue']);
+const handleInput = (event: Event) => {
+  const target = event.target as HTMLInputElement;
+  if (target) {
+    emits('update:modelValue', target.value);
+  }
+};
+</script>
+
+<template>
+    <div class="flex flex-col gap-1.5">
+        <label :for="label">{{ label }}</label>
+        <input class="rounded-lg py-2 px-3 focus:outline-main-blue outline-1 outline outline-[#ABB8C7]" :id="label"
+            :type="type" :placeholder="placeholder" :value="modelValue"
+            @input="handleInput">
+    </div>
+</template>
