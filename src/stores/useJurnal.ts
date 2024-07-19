cf052ba36859e91b7f120086b2ee347a4e907068
azkia ajmal fairuz <azkiajmal@gmail.com>
2024-07-20 02:33:08 +0700

    render data jurnal to page

--- src/stores/useJurnal.ts
+++ src/stores/useJurnal.ts
@@ -1,20 +1,21 @@
 import type { BaseResponse } from "@/type/BaseResponse";
 import type { Jurnal } from "@/type/Jurnal";
+import type { Pagination } from "@/type/Pagination";
 import { getToken } from "@/utils/getToken";
 import axios from "axios";
 import { defineStore } from "pinia";
 
 export const useJurnal = defineStore("jurnal", {
   state: () => ({
-    pagination: {} || undefined,
-    jurnal: {},
+    pagination: {} as Pagination,
+    jurnal: [] as Jurnal[] ,
     error: false,
     message: "",
   }),
   actions: {
     async getAllJurnal() {
       const token = getToken();
-      const response = await axios.get<BaseResponse<Jurnal>>(
+      const response = await axios.get<BaseResponse<Jurnal[]>>(
         `${import.meta.env.VITE_BASE_API}/dosen/jurnal`,
         {
           headers: {
@@ -24,7 +25,10 @@ export const useJurnal = defineStore("jurnal", {
       );
       const data = response.data;
       if (data.data) {
-        this.pagination = data.paginationData;
+        if (data.paginationData) {
+            this.pagination = data.paginationData;
+            
+        }
         this.jurnal = data.data;
         this.message = data.message;
         this.error = false;
--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -2,11 +2,13 @@
 import CardProfile from '@/components/CardProfile.vue';
 import PrimaryButton from '@/components/PrimaryButton.vue';
 import IconHumberger from '@/components/icons/IconHumberger.vue';
-import { onMounted, reactive } from 'vue';
+import { onMounted, reactive, ref } from 'vue';
 import type { Profile } from '@/type/Profile';
 import { useUser } from '@/stores/useProfile';
-import {  getFormattedDate } from "@/utils/formateDate";
+import { getFormattedDate } from "@/utils/formateDate";
 import { getToken } from '@/utils/getToken';
+import { useJurnal } from '@/stores/useJurnal';
+import type { Jurnal } from '@/type/Jurnal';
 const profile = reactive<Profile>({
   nidn: "",
   nama: "",
@@ -19,35 +21,41 @@ const profile = reactive<Profile>({
   alamatSurel: "",
 })
 
+const jurnal = ref<Jurnal[]>([])
 const profileStore = useUser();
+const jurnalStore = useJurnal();
 
 const token = getToken()
 
 async function getProfile() {
   if (token) {
-   await profileStore.getProfile(token);
+    await profileStore.getProfile(token);
     if (profileStore.profile) {
       Object.assign(profile, profileStore.profile);
-      console.log(profile);
     }
   }
+  await jurnalStore.getAllJurnal()
+  if (jurnalStore.jurnal) {
+    jurnal.value = jurnalStore.jurnal
+  }
+
 }
 
 onMounted(() => {
   getProfile()
 })
 async function hanldePresensi() {
-  
+
 }
 
 </script>
 <template>
-     <main class="min-h-svh flex flex-col w-100  bg-main-blue">
+  <main class="min-h-svh flex flex-col w-100  bg-main-blue">
     <div class="flex  p-6 items-center justify-center">
-        <p class="text-[20px] text-center flex-1 text-white">Profile dosen</p>
-        <div class=" bg-black">
-            <IconHumberger/>
-        </div>
+      <p class="text-[20px] text-center flex-1 text-white">Profile dosen</p>
+      <div class=" bg-black">
+        <IconHumberger />
+      </div>
     </div>
     <div class="relative">
       <div class="bg-surface mx-6  flex flex-col gap-6 shadow-custom-card z-50 relative rounded-[12px] p-3">
@@ -66,7 +74,7 @@ async function hanldePresensi() {
       </div>
       <div class="min-h-svh rounded-[24px] p-6  mt-[-62px] bg-[#FBFBFB] z-0 relative">
         <section class="mt-[62px] ">
-          <div class="flex flex-col gap-3">
+          <div class="flex flex-col gap-4">
             <CardProfile title="Biodata">
               <template v-slot:body>
                 <div class="text-[14px] text-main-blue flex flex-col gap-1">
@@ -77,6 +85,26 @@ async function hanldePresensi() {
                   <p>{{ profile.alamatSurel }}</p>
                   <p>{{ profile.noTelephone }}</p>
                 </div>
+              </template>
+            </CardProfile>
+            <CardProfile title="Biodata">
+              <template v-slot:body>
+                <div class="flex flex-col gap-6">
+
+                  <div v-for="item in jurnal" :key="item.id"
+                    class="text-[14px] border-black  text-main-blue flex flex-col gap-1">
+                    <h1 class="text-xs">{{ item.judulArtikel }}</h1>
+                    <p class="text-xs text-main-blue">{{ item.namaJurnal }}</p>
+                    <p class="text-main-blue text-[10px]">Volume {{ item.volume }}, No {{ item.nomor }}, hal {{
+                      item.halaman }},ISSN: {{ item.issn }}, {{ item.tanggalTerbit }}</p>
+                    <div class="w-full flex justify-between items-center">
+                      <a class="text-[#087BFB] text-[10px]" :href="item.tautanLamanJurnal">Link Jurnal</a>
+                      <p class="text-main-blue text-xs cursor-pointer">selengkapnya</p>
+                    </div>
+
+                  </div>
+                </div>
+
               </template>
             </CardProfile>
           </div>
