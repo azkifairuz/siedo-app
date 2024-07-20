0c9e3d9293d74014c51d3b5cb7879b741dfae0ed
azkia ajmal fairuz <azkiajmal@gmail.com>
2024-07-20 22:59:07 +0700

    feat: logic presensi offline

--- src/stores/usePresensi.ts
+++ src/stores/usePresensi.ts
@@ -1,8 +1,44 @@
+import type { BaseResponse } from "@/type/BaseResponse";
+import axios from "axios";
 import { defineStore } from "pinia";
 
 export const usePresensi = defineStore("presensi", {
   state: () => ({
     isActive: false,
-    tok
+    message: "",
+    error: false,
+    isLoading: false,
   }),
+
+  actions: {
+    async presensiOffline(token: string, inLocation: boolean) {
+      this.isLoading = true;
+      try {
+        const response = await axios.post<BaseResponse<string>>(
+          `${import.meta.env.VITE_BASE_API}/dosen/presensi/offline`,
+          {
+            inLocation,
+          },
+          {
+            headers: {
+              Authorization: token,
+            },
+          }
+        );
+        if (response.data.statusCode) {
+          this.isActive = true;
+          this.message = response.data.message;
+          this.error = false;
+        } else {
+          this.error = true;
+          this.message = response.data.message;
+        }
+      } catch (error) {
+        this.error = true;
+        this.message = `error: ${error}`;
+      }finally{
+        this.isLoading = true
+      }
+    },
+  },
 });
--- src/type/BaseResponse.ts
+++ src/type/BaseResponse.ts
@@ -1,7 +1,7 @@
 import type { Pagination } from "./Pagination";
 
 export type BaseResponse<T> = {
-    data: T;
+    data?: T;
     paginationData?: Pagination;
     statusCode: number;
     message: string;
